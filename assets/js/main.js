c7b2ca32cefa4f705bd07b1f48e81d2a1282d0fe
affinoX <groza.t777@gmail.com>
2025-05-19 23:34:13 +0500

    1200px to 992px responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,30 @@
 const swiper1 = new Swiper(".swiper-services", {
   loop: true,
-
   autoplay: {
-    delay: 0, // нет задержки между слайдами
+    delay: 0,
     disableOnInteraction: false,
   },
-  speed: 3000, // скорость прокрутки одного слайда (в мс)
+  speed: 3000,
   slidesPerView: 4,
-  spaceBetween: 24, // расстояние между слайдами
+  spaceBetween: 20,
+
+  breakpoints: {
+    // при ширине <= 1200px
+    1201: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    // при ширине <= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 45,
+    },
+    // при ширине <= 768px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 12,
+    },
+  },
 });
 
 const swiper2 = new Swiper(".porjects-services", {
@@ -20,4 +37,22 @@ const swiper2 = new Swiper(".porjects-services", {
   speed: 3000, // скорость прокрутки одного слайда (в мс)
   slidesPerView: 4,
   spaceBetween: 24, // расстояние между слайдами
+
+  breakpoints: {
+    // при ширине <= 1200px
+    1201: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    // при ширине <= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 45,
+    },
+    // при ширине <= 768px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 12,
+    },
+  },
 });
